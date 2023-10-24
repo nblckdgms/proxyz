4d4ed5e8db2404628e268ad3584365ad8dc33a62
Inchrys <90226439+InchrysUploader@users.noreply.github.com>
2023-10-24 10:27:12 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.addictinggames.com/"; //your website url
+  let target = "https://www.y8.com/"; //your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
