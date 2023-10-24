cdc262564e5b9f25d1d33fdd3d065e35fc753e81
Inchrys <90226439+InchrysUploader@users.noreply.github.com>
2023-10-24 10:16:06 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.crazygames.com/"; //your website url
+  let target = "https://games.crazygames.com/en_US/house-of-hazards/index.html"; //your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
